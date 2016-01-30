260ddfbe94878f97f601fde3410e24b8c7ff2e0e
Michael Selsky <michael@selsky.me>
2016-01-30 11:54:57 -0500

    Added ControllerInput in tvOS project

--- The Beating of the Gates/ControllerInput.swift
+++ The Beating of the Gates/ControllerInput.swift
@@ -7,3 +7,83 @@
 //
 
 import Foundation
+
+import GameController
+import VirtualGameController
+
+let VgcAppIdentifier: String = "BeatingAtTheGates"
+
+class ControllerInput {
+    
+    var controllers: Set<VgcController> = []
+    
+    func start() {
+        startLooking()
+    }
+    
+    func startLooking() {
+        VgcManager.startAs(.Central, appIdentifier: VgcAppIdentifier)
+        VgcController.startWirelessControllerDiscoveryWithCompletionHandler(nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "controllerDidConnect:", name: VgcControllerDidConnectNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector:"controllerDidDisconnect:" , name: VgcControllerDidDisconnectNotification, object: nil)
+        
+    }
+    
+    @objc func controllerDidConnect(note: NSNotification) {
+        guard let controller = note.object as? VgcController else {
+            return
+        }
+        
+        guard setUpController(controller) else {
+            controller.disconnect()
+            return
+        }
+        
+        controllers.insert(controller)
+        
+        guard controllers.count <= 4 else {
+            return
+        }
+        
+        controller.playerIndex = GCControllerPlayerIndex(rawValue: controllers.count)!
+    }
+    
+    @objc func controllerDidDisconnect(note: NSNotification) {
+        guard let controller = note.object as? VgcController else {
+            return
+        }
+        controllers.remove(controller)
+    }
+    
+    func setUpController(controller: VgcController) -> Bool {
+        guard controller.profileType == .Gamepad else {
+            return false
+        }
+        
+        guard let profile = controller.gamepad else {
+            return false
+        }
+        
+        profile.valueChangedHandler = { (gamepad: GCGamepad, element: GCControllerElement) in
+            if gamepad.buttonA == element && gamepad.buttonA.pressed {
+                print("a")
+            }
+            if gamepad.buttonB == element && gamepad.buttonB.pressed {
+                print("b")
+            }
+            if gamepad.buttonX == element && gamepad.buttonX.pressed {
+                print("x")
+            }
+            if gamepad.buttonY == element && gamepad.buttonY.pressed {
+                print("y")
+            }
+            if gamepad.dpad.up == element && gamepad.dpad.up.pressed {
+                print("up")
+            }
+            if gamepad.dpad.down == element && gamepad.dpad.down.pressed {
+                print("down")
+            }
+        }
+        return true
+    }
+}
