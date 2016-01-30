d56c372aa8c2e1c64e11b80b7008892b28214ee4
Michael Selsky <michael@selsky.me>
2016-01-30 17:39:52 -0500

    Gesture Enum

---
+++ Gesture.swift
@@ -0,0 +1,13 @@
+//
+//  Gesture.swift
+//  The Beating at the Gates
+//
+//  Created by MichaelSelsky on 1/30/16.
+//  Copyright © 2016 Grant J. Butler. All rights reserved.
+//
+
+import Foundation
+
+enum Gesture {
+    case Up, Down, Skeleton, Snake, Spider, Pause
+}
