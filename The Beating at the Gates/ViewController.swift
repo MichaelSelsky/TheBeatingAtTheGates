464c57acb4c5cca7ac864275d35156b1ea12bdd2
Grant J. Butler <butler.grant.j@gmail.com>
2016-01-29 22:05:20 -0500

    Initial Xcode project.

---
+++ The Beating at the Gates/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  The Beating at the Gates
+//
+//  Created by Grant Butler on 1/29/16.
+//  Copyright © 2016 Grant J. Butler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
---
+++ The Beating at the GatesUITests/The_Beating_at_the_GatesUITests.swift
@@ -0,0 +1,36 @@
+//
+//  The_Beating_at_the_GatesUITests.swift
+//  The Beating at the GatesUITests
+//
+//  Created by Grant Butler on 1/29/16.
+//  Copyright © 2016 Grant J. Butler. All rights reserved.
+//
+
+import XCTest
+
+class The_Beating_at_the_GatesUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
