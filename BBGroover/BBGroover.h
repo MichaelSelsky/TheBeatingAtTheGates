7220b2d1b16f8161e2717ea366b90648e434f0e8
Grant J. Butler <butler.grant.j@gmail.com>
2016-01-30 21:04:20 -0500

    Make framework for BBGroover.

---
+++ BBGroover/BBGroover.h
@@ -0,0 +1,19 @@
+//
+//  BBGroover.h
+//  BBGroover
+//
+//  Created by Grant Butler on 1/30/16.
+//  Copyright © 2016 Grant J. Butler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BBGroover.
+FOUNDATION_EXPORT double BBGrooverVersionNumber;
+
+//! Project version string for BBGroover.
+FOUNDATION_EXPORT const unsigned char BBGrooverVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BBGroover/PublicHeader.h>
+
+
